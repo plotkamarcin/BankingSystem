df387f7ad47de53214cf15eb519977ee573bc1b8
plotkamarcin <plotkamarcin@wp.pl>
2015-03-21 12:23:52 +0100

    1st commit
    zakladam repo na projekt javy

---
+++ src/Customer.java
@@ -0,0 +1,12 @@
+
+public class Customer {
+
+	private String name;
+	private String surname;
+	private String city;
+	private String street;
+	private int socialId;
+	private int yearOfBirth;
+	boolean isActive=false;
+	private int accountNumber;
+}
