2df02c7c3a24cb2ea5e98c5f039d347b5283b660
plotkamarcin <plotkamarcin@wp.pl>
2015-03-26 10:27:37 +0100

    dodalem okienko

--- src/main/main/BankingAppMain.java
+++ src/main/main/BankingAppMain.java
@@ -3,8 +3,9 @@ package main;
 public class BankingAppMain {
 
 	public static void main(String[] args) {
-		System.out.println("This is the banking app");
-
+		ErrorDialogWindow err = new ErrorDialogWindow("Uruchomi³eœ aplikacjê");
+		err.show();
+     
 	}
 
 }
