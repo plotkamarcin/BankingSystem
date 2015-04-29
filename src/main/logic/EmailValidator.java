20b4af6cb09a4fb69435ff1599b973182d83f7d9
plotkamarcin <plotkamarcin@wp.pl>
2015-04-29 16:07:17 +0200

    dodany walidator przez email

--- src/main/logic/EmailValidator.java
+++ src/main/logic/EmailValidator.java
@@ -1,5 +1,74 @@
 package logic;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+
 public class EmailValidator {
+
+	private Properties mailServerProperties;
+	private Session mailSession;
+	private MimeMessage generateMailMessage;
+	
+	public Properties getMailServerProperties() {
+		return mailServerProperties;
+	}
+
+	public Session getMailSession() {
+		return mailSession;
+	}
+
+	public MimeMessage getGenerateMailMessage() {
+		return generateMailMessage;
+	}
+
+	public EmailValidator() {
+
+	}
+
+	public EmailValidator(Properties props, Session session) {
+		this.mailServerProperties=props;
+		this.mailSession = session;
+	}
+
+	
+	public void sendVerificationEmail(String recipient, String token)
+			throws AddressException, MessagingException {
+		generateMailMessage = new MimeMessage(mailSession);
+		generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+		generateMailMessage.setSubject("BANKING APP VALIDATION TOKEN");
+		String emailBody = "This is your login verification token: "+token;
+		generateMailMessage.setContent(emailBody, "text/html");
+		prepareTransportSession();
+	
+	}
+
+	public void setMailServerProperties() throws AddressException, MessagingException {
+		mailServerProperties = System.getProperties();
+		mailServerProperties.put("mail.smtp.port", "587");
+		mailServerProperties.put("mail.smtp.auth", "true");
+		mailServerProperties.put("mail.smtp.starttls.enable", "true");
+        setMailSession();
+	}
+	
+	public void setMailSession() throws AddressException, MessagingException{
+		mailSession = Session.getDefaultInstance(mailServerProperties, null);
+		
+	}
+	private void prepareTransportSession() throws MessagingException{
+		Transport transport = mailSession.getTransport("smtp");	
+		transport.connect("smtp.gmail.com", "thebankingapp2015@gmail.com", "testowaJAVA");
+		transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+		transport.close();
+	}
 
 }
---
+++ src/test/logic/EmailValidatorTest.java
@@ -0,0 +1,55 @@
+package logic;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.isNotNull;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+
+
+
+
+
+import javax.mail.internet.AddressException;
+
+import org.hamcrest.core.IsNot;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailValidatorTest {
+	
+	private EmailValidator emailValidator;
+    @Before
+    public void init(){
+    	emailValidator= new EmailValidator();
+    }
+
+	@Test
+	public void shouldHaveSetAllSmtpPortProperties() throws AddressException, MessagingException {
+		emailValidator.setMailServerProperties();
+		assertThat(emailValidator.getMailServerProperties(),hasEntry("mail.smtp.port", "587"));
+		assertThat(emailValidator.getMailServerProperties(),hasEntry("mail.smtp.auth", "true"));
+		assertThat(emailValidator.getMailServerProperties(),hasEntry("mail.smtp.starttls.enable", "true"));
+	}
+	
+	@Test
+	public void shouldSetMailSession() throws AddressException, MessagingException{
+		emailValidator.setMailServerProperties();
+		emailValidator.setMailSession();
+		assertThat(emailValidator.getMailSession().toString().length(),greaterThan(0));
+	}
+
+
+	@Test
+	public void testSendVerificationEmail() throws AddressException, MessagingException {
+		//emailValidator.setMailServerProperties();
+		//emailValidator.sendVerificationEmail("igor.avz@gmail.com", "testToken");
+	}
+
+}
